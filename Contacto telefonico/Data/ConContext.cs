15bb0ea289822f85cecf6f26ae2a2ee2a20d559b
Arturo Hernandez Carmona <arturo_hernandez@tesh.edu.mx>
2020-05-18 18:22:15 -0500

    Modelo de datos creado

---
+++ Contacto telefonico/Data/ConContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Contacto_telefonico.Data
+{
+    public class ConContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ConContext() : base("name=ConContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Contacto_telefonico.Models.Contactos> Contactos { get; set; }
+    }
+}
---
+++ Contacto telefonico/Models/Contactos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contacto_telefonico.Models
+{
+    public class Contactos
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Compania { get; set; }
+        public String Titulo { get; set; }
+        public String Telefono { get; set; }
+        public String CorreoElectronico{ get; set; }
+    }
+}
